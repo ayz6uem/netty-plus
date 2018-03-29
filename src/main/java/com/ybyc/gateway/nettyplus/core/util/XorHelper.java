60899113b36994d8db23cb4f5f29d19532b36956
wangzhe <wangzhe>
2018-03-29 11:36:12 +0800

    使用原码进行xor校验

--- src/main/java/com/ybyc/gateway/nettyplus/core/util/XorHelper.java
+++ src/main/java/com/ybyc/gateway/nettyplus/core/util/XorHelper.java
@@ -14,7 +14,7 @@ public class XorHelper {
     public static byte loop(ByteBuf buf,int start, int end) {
         byte xor = 0x00;
         for (int i = start; i < end; i++) {
-            xor ^= buf.getByte(i);
+            xor ^= buf.getByte(i) & 0xFF;
         }
         return xor;
     }
